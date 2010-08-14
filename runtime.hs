5195a9665dde28721435489a6f66c69428e5c2b7
Sebastian Fischer <sebf@informatik.uni-kiel.de>
2010-08-14 12:24:49 +0200

    Use infinite list type for inifinite lists.

--- runtime.hs
+++ runtime.hs
@@ -2,6 +2,6 @@ import Criterion.Main
 import Data.Numbers.Primes
 
 main :: IO ()
-main = defaultMain [ test size | size <- [5..15] ]
+main = defaultMain [ test size | size <- [0..9] ]
  where test size = bench ("wheel"++show size) $ whnf comp size
        comp size = wheelSieve size !! 10000
